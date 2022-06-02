1aeb1ca117949aa4fdaee11dd03aafc49abdebe1
auz <lankyauz@gmail.com>
2022-06-02 15:01:29 -0600

    Added a few basic validations to the html. Added a few CSS styles to help communicate to the user if their input is properly formatted. Started working on a bit of javascript to change the password inputs border colors depending on if their input matches each other.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function passMatch(){
+    let pass = document.querySelector = "#passInput";
+    let passconfirm = document.querySelector = "#passConfirmationInput";
+    
+};
+
+// display red borders until the passwords match.
