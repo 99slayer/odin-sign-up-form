757c30bf977830727e5b076fa2f280e550ac2ccc
auz <lankyauz@gmail.com>
2022-06-03 13:35:22 -0600

    Password matching JS is mostly working.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let pass = document.querySelector("#passInput");
+let passConfirm = document.querySelector("#passConfirmationInput");
+pass.addEventListener("input", passMatch)
+passConfirm.addEventListener("input", passMatch)
 
 function passMatch(){
-    let pass = document.querySelector = "#passInput";
-    let passconfirm = document.querySelector = "#passConfirmationInput";
-    
+    // let pswd = pass.value;
+    if(passConfirm.value==pass.value){
+        passConfirm.style.border = "2px solid green";
+    }
+    else{
+        passConfirm.style.border = "2px solid red";
+    };
 };
-
-// display red borders until the passwords match.
